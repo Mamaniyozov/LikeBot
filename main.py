d2e34a64fc756c9cf02f84ab37883e9cb61ace73
ZARIFMAC <uzbzarif@gmail.com>
2023-01-23 10:27:39 +0500

    start

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import os
 
 TOKEN = os.environ['TOKEN']
 
+
 def start(update: Update,context: CallbackContext):
-    update.message.reply_text("Hello World")
+    bot = context.bot
+    # group_id = 1898707587
+    bot.send_message(chat_id='5575549228',text="Hello World")
+    bot.send_message(chat_id='@image_like',text="Hello World")
+   
+    # update.message.reply_text("Hello World")
 
 
 updater = Updater(token=TOKEN)
