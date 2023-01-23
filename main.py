936fccd4bf5f15afddebfa0f6b8791799244aada
ZARIFMAC <uzbzarif@gmail.com>
2023-01-23 10:35:11 +0500

    Send image to channel

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ TOKEN = os.environ['TOKEN']
 
 def start(update: Update,context: CallbackContext):
     bot = context.bot
-    # group_id = 1898707587
-    bot.send_message(chat_id='5575549228',text="Hello World")
     bot.send_message(chat_id='@image_like',text="Hello World")
-   
-    # update.message.reply_text("Hello World")
+    update.message.reply_text("Hello World")
 
+def sendImage(update: Update,context: CallbackContext):
+    bot = context.bot
+    # Get image id from update
+    image_id = update.message.photo[-1].file_id
+
+    
+    bot.send_photo(chat_id='@image_like',photo=image_id,caption="Hello World")
+    update.message.reply_text("Image has been sent to @image_like")
 
 updater = Updater(token=TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler('start',start))
+updater.dispatcher.add_handler(MessageHandler(Filters.photo,sendImage))
 
 updater.start_polling()
 updater.idle()
